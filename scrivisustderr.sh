f70c63845d4953c8c05c4471ce3607899a241bbf
Rebecca Scarcelli <158066756+R-Scarcelli@users.noreply.github.com>
2024-12-11 17:40:23 +0100

    Add files via upload

---
+++ scrivisustderr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+### Scrivere uno script scrivisustderr.sh che legge delle righe passategli sullo
+### standard input fino a che non incontra l’EOF. Le righe passate sullo stdin devono
+### essere composte da parole separate da spazi. Per ciascuna riga lo script usa i
+### comandi di estrazione di stringhe per estrarre la prima parola (separata da spazi
+### bianchi) contenuta nella riga. Quella parola estratta deve essere scritta, mediante il
+### comando echo, sullo standard error.
+### Per ciascuna parola scritta sullo stderr, deve
+### essere scritta la stringa evviva sullo standard output.
+### Provare poi a passare, come input da tastiera, una riga con un * come prima
+### parola.
+
+while read FIRST ALTRO ; do
+	echo ${FIRST} 1>&2
+	echo "evviva"
+done

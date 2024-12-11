c0fbc20fe528e652728d3d7957767611829f8378
Rebecca Scarcelli <158066756+R-Scarcelli@users.noreply.github.com>
2024-12-11 20:44:16 +0100

    Add files via upload

---
+++ es21.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+### Stampare a video le sole righe del file /usr/include/stdio.h che NON contengono
+### alcun asterisco *
+### Suggerimento2: Guardare il man di grep per capire come usare grep in modo
+### opportuno
+
+grep -v "\*" /usr/include/stdio.h

591dec31cd77d62d5b318aa1d3d2238db3993055
Rebecca Scarcelli <158066756+R-Scarcelli@users.noreply.github.com>
2024-12-11 17:29:06 +0100

    Upgrading separanomi.sh

--- separanomi.sh
+++ separanomi.sh
@@ -6,27 +6,6 @@
 # PATH="/bin:/usr/bin:/usr/sbin" , lo script dovrà mettere in output 4 righe contenenti
 # rispettivamente bin usr bin usr sbin
 
-#!/bin/bash
-RESIDUO=${PATH}
-# Continuo fino a che la stringa non è vuota
-while [[ -n ${RESIDUO} ]] ; do
-#COnsidero il primo carattere
-	PRIMOCHAR=${RESIDUO:0:1}
-# Se è / o * lo taglio
-	if [[ ${PRIMOCHAR} == "/" || ${PRIMOCHAR} == ":" ]] ; then
- 		RESIDUO=${RESIDUO:1}
-	else
-# Se non è così considero la stringa più corta fino al prossimo / o * e la stampo
- 		PEZZO1=${RESIDUO%%:*}
- 		PEZZO2=${RESIDUO%%/*}
- 		if (( ${#PEZZO1} < ${#PEZZO2} )) ; then
- 			PEZZO=${PEZZO1}
- 		else
- 			PEZZO=${PEZZO2}
-	 	fi
- 		echo ${PEZZO}
-# Aggiorno la posizione alla lunghezza del pezzo stampato più 1
- 		OFFSET=$(( ${#PEZZO}+1 ))
- 		RESIDUO=${RESIDUO:${OFFSET}}
-	fi
-done
+P=${PATH//\//\\n}
+P=${P//:/}
+echo -e "${P}"
